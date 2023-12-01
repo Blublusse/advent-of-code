1fc99201d699b2360c0cb012fe5714cc9984a5b4
Thomas Blusseau <thomas.blusseau@kelio.com>
2023-12-01 14:15:56 +0100

    fix tests

--- lib/aoc_solver.py
+++ lib/aoc_solver.py
@@ -25,8 +25,7 @@ class AOCSolver:
 
     puzzle2_input = (self.dir / "input2.txt").read_text().strip()
     solution2 = self.solve_part2(self.parse_input(puzzle2_input))
-    print("solution 2 : \033[38;5;3m" + str(solution2) + "\033[0;0m")
-    print("\n")
+    print("solution 2 : \033[38;5;3m" + str(solution2) + "\033[0;0m\n")
 
   def test(self):
     print("\n# Tests ###########################")
@@ -34,8 +33,11 @@ class AOCSolver:
       for file in os.listdir(self.dir / "examples/" / str(part)):
         if "input" in file:
           example = file[0:2]
-          example1_input = (self.dir / "examples" / str(part) / file).read_text().strip()
-          example1_solution = self.solve_part1(self.parse_input(example1_input))
+          example_input = (self.dir / "examples" / str(part) / file).read_text().strip()
+          if part == 1:
+            example_solution = self.solve_part1(self.parse_input(example_input))
+          else:
+            example_solution = self.solve_part2(self.parse_input(example_input))
           solution_file = file[0:2] + "_solution.txt"
           example1_expected_solution = (self.dir / "examples" / str(part) / solution_file).read_text().strip()
-          print(str(part) + "/" + example + " : " + ("\033[38;5;2mOK\033[0;0m" if str(example1_solution) == example1_expected_solution else "\033[38;5;1mNOK\033[0;0m"))
+          print(str(part) + "/" + example + " : " + ("\033[38;5;2mOK\033[0;0m" if str(example_solution) == example1_expected_solution else "\033[38;5;1mNOK\033[0;0m"))
